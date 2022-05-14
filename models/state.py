64e23a636dd145573b5fecab9bd2c907be773aad
AnniesTech <anita.rocha0505@gmail.com>
2022-05-14 16:09:11 -0500

    Half of task 6, missing DBStorage file

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import storage
+from models.city import City
+import models
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    if models.storage_t == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="states", cascade="all")
+    else:
+        name = ""
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """Returns a list of City instances with state_id equals
+            to the current State.id"""
+            citylist = []
+            allcitys = storage.all(City)
+            for city in allcitys.values():
+                if city.state_id == self.id:
+                    citylist.append(city)
+            return citylist
