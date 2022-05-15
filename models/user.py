9abb79b081c2db36f1ce86a69969b0911e7f7ae5
Daniel1375 <danydu1299@gmail.com>
2022-05-15 17:02:54 -0500

    revert changes

--- models/user.py
+++ models/user.py
@@ -4,20 +4,11 @@ import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
-from models.city import Place
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if models.storecondition == "db":
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
