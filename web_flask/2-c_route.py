0c6ae3135f7969ae86f2c4e731d12997f5d67c7d
Daniel1375 <danydu1299@gmail.com>
2022-05-24 16:59:36 -0500

    flask app with /c/<text> route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Routes """
+
+from flask import Flask
+
+"""Create an instance from app.create_app()"""
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """
+    Route index
+    """
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """
+    Route index
+    """
+
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_route(text):
+    """
+    Route index
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True, host='0.0.0.0')
