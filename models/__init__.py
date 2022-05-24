5ca73716a49770659fc12a841e78a73721eb81d0
Daniel1375 <danydu1299@gmail.com>
2022-05-24 17:59:41 -0500

    update all

--- models/__init__.py
+++ models/__init__.py
@@ -9,7 +9,9 @@ storecondition = getenv("HBNB_TYPE_STORAGE")
 if storecondition == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    # print("In dbStorage")
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+    # print("In file_storage")
 storage.reload()
