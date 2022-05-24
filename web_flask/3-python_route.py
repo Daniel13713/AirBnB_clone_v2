be386c0413dfef3ce837eb6538b48a8f91835080
Daniel1375 <danydu1299@gmail.com>
2022-05-24 17:08:54 -0500

    flask app with /python/<text> route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Routes """
+
+from flask import Flask
+
+"""Create an instance from app.create_app()"""
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """
+    Route index
+    """
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """
+    Route index
+    """
+
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_route(text):
+    """
+    Route index
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_route(text="is cool"):
+    """
+    Route index
+    """
+    text = text.replace("_", " ")
+    return "Pyhton {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True, host='0.0.0.0')
